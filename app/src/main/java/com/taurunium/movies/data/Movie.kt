22f40f4c8b869646b2857de6d02ab9eb90655c5c
DESKTOP-N4VRD92\Fujitsu <vidojemuric@gmail.com>
2023-09-22 16:52:03 +0300

    Added dependencies. Added Data classes

---
+++ app/src/main/java/com/taurunium/movies/data/Movie.kt
@@ -0,0 +1,20 @@
+package com.taurunium.movies.data
+
+import com.google.gson.annotations.SerializedName
+
+data class Movie(
+    @SerializedName("id")
+    val movieId: Int,
+
+    @SerializedName("overview")
+    val overview: String,
+
+    @SerializedName("poster_path")
+    val posterPath: String,
+
+    @SerializedName("release_date")
+    val releaseDate: String,
+
+    @SerializedName("title")
+    val title: String
+)
