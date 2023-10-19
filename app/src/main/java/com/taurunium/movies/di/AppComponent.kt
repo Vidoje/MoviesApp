2dcb0b50cc50390a1e3292bb6ffaf57d4526b3cc
DESKTOP-N4VRD92\Fujitsu <vidojemuric@gmail.com>
2023-10-19 09:18:40 +0300

    Dependency Injection - App component modules

---
+++ app/src/main/java/com/taurunium/movies/di/AppComponent.kt
@@ -0,0 +1,22 @@
+package com.taurunium.movies.di
+
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component(
+    modules = [
+        AppModule::class,
+        CacheDataModule::class,
+        DatabaseModule::class,
+        LocalDataModule::class,
+        MovieModule::class,
+        MovieRepositoryModule::class,
+        NetModule::class,
+        RemoteDataModule::class,
+        UseCaseModule::class
+    ]
+)
+interface AppComponent {
+    fun movieSubComponent(): MovieSubComponent.Factory
+}
